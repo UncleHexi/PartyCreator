1689673a41bd1df99b59e329d46dc28a02d4e059
Michal Boese <boesem93@gmail.com>
2023-10-20 00:06:22 +0200

    nowelogowanie

---
+++ PartyCreatorFrontEnd/PartyCreatorClient/src/app/login/login.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+export type LoginType = 'signin' | 'signup';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+})
+
+// wybór między logowaniem a rejestracją
+export class LoginComponent {
+  //ustawianie domyślnego formularza
+  login: LoginType = 'signup';
+
+  get showSignInForm() {
+    return this.login === 'signin';
+  }
+
+  get showSignUpForm() {
+    return this.login === 'signup';
+  }
+
+  toggleForm(type: LoginType) {
+    this.login = type;
+  }
+}
