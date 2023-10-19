8809dfbad0cd8683e213d0d51fef8e2ea8658d78
Michal Boese <boesem93@gmail.com>
2023-10-19 18:04:23 +0200

    logowanie

--- PartyCreatorFrontEnd/PartyCreatorClient/src/app/app-routing.module.ts
+++ PartyCreatorFrontEnd/PartyCreatorClient/src/app/app-routing.module.ts
@@ -2,15 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestComponent } from './test/test.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }, 
+  { path: '', component: HomeComponent },
   { path: 'test/:id', component: TestComponent },
+  { path: 'konto/:choice', component: LoginComponent },
   // Inne trasy
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- PartyCreatorFrontEnd/PartyCreatorClient/src/app/app.module.ts
+++ PartyCreatorFrontEnd/PartyCreatorClient/src/app/app.module.ts
@@ -7,13 +7,19 @@ import { FormsModule } from '@angular/forms';
 import { TestComponent } from './test/test.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './login/signup/signup.component';
+import { SigninComponent } from './login/signin/signin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    SignupComponent,
+    SigninComponent
   ],
   imports: [
     BrowserModule,
