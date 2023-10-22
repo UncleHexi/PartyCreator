8d5411bf84836709d256925cd5c2ae81d0e4c22c
Adam Rekruciak <adamrekruciak5@gmail.com>
2023-10-22 17:31:20 +0200

    emotikony

--- PartyCreatorFrontEnd/PartyCreatorClient/src/app/home/home.component.ts
+++ PartyCreatorFrontEnd/PartyCreatorClient/src/app/home/home.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import * as signalR from '@microsoft/signalr';
+//icons
+import { faSpotify } from '@fortawesome/free-brands-svg-icons';
+import { faAnglesDown, faCalendarPlus, faPhotoFilm, faMoneyBillTransfer, faUserPlus, faSquarePollVertical, faPersonCircleQuestion, faListCheck, faComments } from '@fortawesome/free-solid-svg-icons';
+//icons
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
   title = 'PartyCreatorClient';
-  //SignalR
-  //Mariusz
-  //Michal
+  //icons
+  faSpotify = faSpotify; //ikonka spotify
+  faMoney = faMoneyBillTransfer;
+  faCalendar = faCalendarPlus;
+  faPhoto = faPhotoFilm;
+  faUser = faUserPlus;
+  faPoll = faSquarePollVertical;
+  faPerson = faPersonCircleQuestion;
+  faList = faListCheck;
+  faComments = faComments;
+  faArrowDown = faAnglesDown;
+  //end of icons
   private hubConnection!: signalR.HubConnection;
   user = '';
   message = '';
