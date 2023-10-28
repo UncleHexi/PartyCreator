5fda29027f014013950caa7cdde7073d9bbab65c
Adam Rekruciak <adamrekruciak5@gmail.com>
2023-10-28 20:48:24 +0200

    strona glowna po zalogowaniu

--- PartyCreatorFrontEnd/PartyCreatorClient/src/app/nav-menu-main/nav-menu-main.component.ts
+++ PartyCreatorFrontEnd/PartyCreatorClient/src/app/nav-menu-main/nav-menu-main.component.ts
@@ -1,10 +1,26 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
+
+//icons
+import { faBell } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav-menu-main',
   templateUrl: './nav-menu-main.component.html',
   styleUrls: ['./nav-menu-main.component.css']
 })
-export class NavMenuMainComponent {
 
+export class  NavMenuMainComponent{
+
+  faBell = faBell;
+
+  isExpanded = false;
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
