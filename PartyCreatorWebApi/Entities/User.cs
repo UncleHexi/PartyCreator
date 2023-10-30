b03c6d5e7d3051cae96c159e61141afa27c5f593
Michal Boese <boesem93@gmail.com>
2023-10-30 15:59:11 +0100

    EntityFramework

---
+++ PartyCreatorWebApi/Entities/User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+
+namespace PartyCreatorWebApi.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Birthday { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public byte[] PasswordHash { get; set; } = new byte[256];
+        public byte[] PasswordSalt { get; set; } = new byte[256];
+
+    }
+}
