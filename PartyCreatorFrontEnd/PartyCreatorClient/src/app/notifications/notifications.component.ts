726d18b1ad9d05aa3b4f9bf958d216d2a977c5c6
Adam Rekruciak <adamrekruciak5@gmail.com>
2023-10-30 17:56:05 +0100

    powiadomienia

--- PartyCreatorFrontEnd/PartyCreatorClient/src/app/notifications/notifications.component.ts
+++ PartyCreatorFrontEnd/PartyCreatorClient/src/app/notifications/notifications.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import { faX } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css']
 })
 export class NotificationsComponent {
-
+  faX = faX;
 }
