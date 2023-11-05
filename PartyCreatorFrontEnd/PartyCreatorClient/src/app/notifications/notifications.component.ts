3c2fc1f674c15bd244b1b958b653158620a028b3
Adam Rekruciak <adamrekruciak5@gmail.com>
2023-11-05 21:00:52 +0100

    poprawiony wyglad strony

--- PartyCreatorFrontEnd/PartyCreatorClient/src/app/notifications/notifications.component.ts
+++ PartyCreatorFrontEnd/PartyCreatorClient/src/app/notifications/notifications.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 import { faX } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
@@ -10,7 +11,10 @@ export class NotificationsComponent {
   
   isVisible: boolean = true;
 
+  @Output() toggleNotifications = new EventEmitter<boolean>();
+
   hideNotifications() {
     this.isVisible = false;
+    this.toggleNotifications.emit(false); // Przekazuje informację, że powiadomienia są ukryte
   }
 }
